546c241e160639841bf080a579cac0a81a0e61a7
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-07-27 08:35:38 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import glslify from "rollup-plugin-glslify";
+
+const root = "";
+
+export default defineConfig({
+  root,
+  base: "/",
+  plugins: [
+    glslify({
+      compress(code) {
+        // Based on https://github.com/vwochnik/rollup-plugin-glsl
+        // Modified to remove multiline comments. See #16
+        let needNewline = false;
+        return code
+          .replace(/\\(?:\r\n|\n\r|\n|\r)|\/\*.*?\*\/|\/\/(?:\\(?:\r\n|\n\r|\n|\r)|[^\n\r])*/gs, "")
+          .split(/\n+/)
+          .reduce((result, line) => {
+            line = line.trim().replace(/\s{2,}|\t/, " "); // lgtm[js/incomplete-sanitization]
+            if (line.charAt(0) === "#" || /else/.test(line)) {
+              if (needNewline) {
+                result.push("\n");
+              }
+              result.push(line, "\n");
+              needNewline = false;
+            } else {
+              result.push(line.replace(/\s*({|}|=|\*|,|\+|\/|>|<|&|\||\[|\]|\(|\)|-|!|;)\s*/g, "$1"));
+              needNewline = true;
+            }
+            return result;
+          }, [])
+          .join(process.env.NODE_ENV === "development" ? "\n" : "")
+          .replace(/\n+/g, "\n");
+      },
+    }),
+  ],
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        index: resolve(root, "index.html"),
+      },
+    },
+  }
+});
