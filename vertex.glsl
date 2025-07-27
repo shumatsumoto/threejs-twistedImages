02b4e9e15a74241395bee1f3e75e0face52b8c03
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-07-27 19:32:50 +0900

    glslify

--- vertex.glsl
+++ vertex.glsl
@@ -1,11 +1,23 @@
 precision mediump float;
 
+#pragma glslify: easeBack = require(glsl-easings/back-in-out)
+
 varying vec2 vUv;
 varying float vDelay;
 attribute float aDelay;
 
+uniform float uProgress;
+
 void main() {
 	vUv = uv;
 	vDelay = aDelay;
-	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+	vec3 pos = position;
+	float delay = distance(vec2(0., 1.), uv) / distance(vec2(0., 1.), vec2(1., 0.));
+
+	float x = clamp(uProgress * 1.3 - delay * 0.3, 0., 1.);
+	float progress = easeBack(x);
+	pos.z += progress * 100.0;
+
+	gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
